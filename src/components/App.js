6d639b823cd9e89b97807e745c2006c6fe5f79aa
Ashley Lampard <ashleylampard@live.com>
2022-06-23 15:43:34 +0100

    fixed html error

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import youtube from "../apis/youtube";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
@@ -7,25 +6,23 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-
   const [videos, search] = useVideos("dog");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
   }, [videos]);
 
-  // setSelectedVideo(response.data.items[0]);
-
   return (
     <div className="ui container">
       <SearchBar onFormSubmit={search} />
       <div className="ui grid">
-        <div className="ui row"></div>
-        <div className="eleven wide column">
-          <VideoDetail video={selectedVideo} />
-        </div>
-        <div className="five wide column">
-          <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,12 +3,7 @@ import "./VideoItem.css";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div
-      onClick={() => {
-        onVideoSelect(video);
-      }}
-      className="video-item item"
-    >
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
